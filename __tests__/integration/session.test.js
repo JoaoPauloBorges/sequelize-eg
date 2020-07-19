12eb0c52da65054b6e592896b052b5372f530bd6
João Paulo Borges <joaopaulo.borges.gm@hotmail.com>
2020-07-19 15:56:49 -0300

    removing sqlite db from git

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -2,11 +2,16 @@ const loader = require('../../src/loaders/sequelize');
 const User = require('../../src/models/User');
 
 describe('Authentication', () => {
+
+    beforeEach(() => console.log('before each')
+    );
+
     it('should sum two numbers', async () => {
         loader();
         const user = await User.create({ name: 'Joao', email: 'joao@gmail.com', password_hash: '123123' });
-        console.log(user);
 
         expect(user.name).toBe('Joao');
     });
+
+    afterAll(() => { console.log('after all') });
 });
